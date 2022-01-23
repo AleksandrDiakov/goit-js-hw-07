012773d0d667e6d01275a64cf7dfc91281759cf1
Aleksandr Diakov <aldiakov@gmail.com>
2022-01-23 13:30:31 +0200

    task-1 done (maybe)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,39 +1,7 @@
 // Задание 1 - галерея изображений
-// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-// Реализация делегирования на div.gallery и получение url большого изображения.
-// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-// Замена значения атрибута src элемента < img > в модальном окне перед открытием.Используй готовую 
-// разметку модального окна с изображением из примеров библиотеки basicLightbox.
-// Разметка элемента галереи
-// Ссылка на оригинальное изображение должна храниться в data - атрибуте source на элементе < img >, и 
-// указываться в href ссылки.Не добавляй другие HTML теги или CSS классы кроме тех, что есть в этом
-// шаблоне.
-
-// <div class="gallery__item">
-//   <a class="gallery__link" href="large-image.jpg">
-//     <img
-//       class="gallery__image"
-//       src="small-image.jpg"
-//       data-source="large-image.jpg"
-//       alt="Image description"
-//     />
-//   </a>
-// </div>
-//     Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию
-// пользователь будет перенаправлен на другую страницу.Запрети это поведение по умолчанию.
-
-// Закрытие с клавиатуры
-// Следующий функционал не обязателен при сдаче задания, но будет хорошей дополнительной практикой.
-
-// Добавь закрытие модального окна по нажатию клавиши Escape.Сделай так, чтобы прослушивание клавиатуры 
-// было только пока открыто модальное окно.У библиотеки basicLightbox есть метод для программного 
-// закрытия модального окна.
-    
 
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
 
 const galleryRef = document.querySelector('.gallery');  
 const galleryMakrup = createGalleryMarkup(galleryItems); 
@@ -48,13 +16,40 @@ function createGalleryMarkup(gelleryItem) {
         <div class="gallery__item">
             <a class="gallery__link" href="${original}">
                 <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
                 />
             </a>
         </div>`;
     })
         .join(``);
 }
+
+// Обработка события Click
+function onGalleryItemClick(evt) {
+    const isImageEl = evt.target.classList.contains('gallery__image');
+    const imageSrc = evt.target.dataset.source;
+    
+// запрещаю открытие на новой сранице
+    evt.preventDefault();
+    if (!isImageEl) {
+        return;
+    }
+    
+// добавил изображение в basicLightbox для отображения на экране
+    const instance  = basicLightbox.create(`<img src="${imageSrc}">`)
+    instance.show()
+
+// закрытие модального окна по нажатию клавиши Escape
+    document.addEventListener("keydown", evt => {
+        if (evt.key != 'Escape') {
+            return;
+        };
+    instance.close()
+    });   
+}
+
+console.log(galleryItems);
+
