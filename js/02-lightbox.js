9ba4f101ad0586158e6974468a13ad0fd4ac4504
Aleksandr Diakov <aldiakov@gmail.com>
2022-01-23 20:49:06 +0200

    task-2
    add SimpleLightbox
    create galleryMarkup

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
+// Задание 2 - библиотека SimpleLightbox
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>`
+    })
+    .join('');
+}
+
+let lightboxGallery = new SimpleLightbox('.gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+    lightboxGallery.on('show.simplelightbox');
 
 console.log(galleryItems);
